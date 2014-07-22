7147f46a7941a6270f4f5d4bd79abd8d9a340866
Thomas Stevens <tvsteven@gmail.com>
2014-07-22 13:04:19 -0700

    First run of naive bayes.

--- naive-bayes.py
+++ naive-bayes.py
@@ -14,10 +14,15 @@ import datetime
 import time
 import sys
 
-def main(output_file=time.strftime('%h%d-%Hh%Mm'), in_pkl):
+logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.NOTSET)
+
+def main(output_file=time.strftime('%h%d-%Hh%Mm')+'.csv', in_pkl=None):
     """ Generates features and fits classifier. 
     Input command line argument is optional run name, defaults to date/time.
     """
+    logging.info("Loading features...")
+    if not in_pkl:
+        return "input .plk required"
     trainFeatures, trainTargets, trainItemIds, testFeatures, testItemIds = joblib.load(in_pkl)
     logging.info("Loaded features, fitting model...")
     # Bernoulli Naive Bayes
@@ -31,14 +36,14 @@ def main(output_file=time.strftime('%h%d-%Hh%Mm'), in_pkl):
     predicted_scores = clf.predict_log_proba(testFeatures).T[1]
 
     logging.info("Write results...")
-    output_file = "output_name.csv"
     logging.info("Writing submission to %s" % output_file)
-    f = open(os.path.join(dataFolder,output_file), "w")
+    f = open(output_file, "w")
     f.write("id\n")
 
     for pred_score, item_id in sorted(zip(predicted_scores, testItemIds), reverse = True):
-    # only writes item_id per output spec, but may want to look at predicted_scores
-    f.write("%d\n" % (item_id))
+        # only writes item_id per output spec, but may want to look at predicted_scores
+        f.write("%d\n" % (item_id))
+
     f.close()
     logging.info("Done.")
 
@@ -50,5 +55,3 @@ if __name__=="__main__":
         print "Not enough args"
     tend = time.time()
     print sys.argv[0]+"time H:M:S = "+str(datetime.timedelta(seconds=tend-tstart))
-
-

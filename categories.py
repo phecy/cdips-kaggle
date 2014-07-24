8e1296def2f0d05860fe49d1f02ee7c88575ceb4
Thomas Stevens <tvsteven@gmail.com>
2014-07-23 17:28:54 -0700

    Corrected category subset indexing.

--- categories.py
+++ categories.py
@@ -2,6 +2,7 @@
 '''
 Use pandas pivots to understand categories and json fields
 '''
+import ipdb
 import numpy as np
 import os
 import pandas as pd
@@ -18,23 +19,26 @@ def indicator(df,label,noncollinear=False):
         dummy = dummy.drop(dummy.columns[-1])
     return sparse.csr_matrix(dummy)
 
-def train(dummy,data_folder,output_file):
+def classify(dummy_train,dummy_test,data_folder,output_file):
     # Train classifier, iterating over subsets
     # Load Features
     print 'Loading features...'
     trainFeatures, trainTargets, trainItemIds, testFeatures, testItemIds = joblib.load(os.path.join(data_folder,"train_data.pkl"))
-    predicted_scores = []
+    trainTargets = np.array(trainTargets)
+    testItemIds = np.array(testItemIds)
     predicted_ids = []
+    predicted_scores = []
     # SGD Logistic Regression per sample 
     clf = SGDClassifier(loss="log",penalty="l2",alpha=1e-4,class_weight="auto")
-    for col in range(np.shape(dummy)[1]):
+    for col in range(np.shape(dummy_train)[1]):
         # Get nonzero dummy indices as array
-        idx = dummy[:,col].astype('bool').T.toarray()[0]
-        print 'Training subset {} of {}...\n'.format(col,np.shape(dummy)[1])
-        clf.fit(trainFeatures[idx,:],trainTargets[idx,:])
+        idx_train = dummy_train[:,col].astype('bool').T.toarray()[0]
+        print 'Training subset {} of {}...'.format(col,np.shape(dummy_train)[1])
+        clf.fit(trainFeatures[idx_train,:],trainTargets[idx_train])
        # Use probabilities instead of binary class prediction in order to generate a ranking    
-        predicted_scores += list(clf.predict_proba(testFeatures[idx,:]).T[1])
-        predicted_ids += testItemIds[idx,:]
+        idx_test = dummy_test[:,col].astype('bool').T.toarray()[0]
+        predicted_scores += clf.predict_proba(testFeatures[idx_test,:]).T[1].tolist()
+        predicted_ids += testItemIds[idx_test].tolist()
     
     with open(os.path.join(data_folder,output_file),'w') as out_fid:
         out_fid.write("id\n")
@@ -42,19 +46,20 @@ def train(dummy,data_folder,output_file):
            # only writes item_id per output spec, but may want to look at predicted_scores
             out_fid.write("%d\n" % (item_id))
 
-def main(in_file='avito_train.tsv',data_folder='new-feat-full/'):
-   print 'Loading categories data frame...'
-   df = pd.read_csv(in_file, sep='\t', usecols=np.array([1,2]))
+def main(train_file='avito_train.tsv',test_file='avito_test.tsv',data_folder='new-feat-full/'):
+   print 'Loading categories data frames...'
+   df_train = pd.read_csv(train_file, sep='\t', usecols=np.array([1,2]))
+   df_test = pd.read_csv(test_file, sep='\t', usecols=np.array([1,2]))
    # category
    print '_SPLIT CATEGORY_'
-   train(indicator(df,'category'),data_folder,'category_split.csv')
+   classify(indicator(df_train,'category'),indicator(df_test,'category'),data_folder,'category_split.csv')
    # subcategory
    print '_SPLIT SUBCATEGORY_'
-   train(indicator(df,'subcategory'),data_folder,'subcategory_split.csv')
+   classify(indicator(df_train,'subcategory'),indicator(df_test,'subcategory'),data_folder,'subcategory_split.csv')
    # One classifier, but with subcategory as feature
    # print '_SUBCATEGORY AS FEATURE_'
-   # indicator(df,'subcategory')
-   # train(np.matrix([0]),data_folder,'subcategory_feature.csv',noncollinear=True)
+   # indicator(df_train,'subcategory')
+   # classify(np.matrix([0]),data_folder,'subcategory_feature.csv',noncollinear=True)
 
 if __name__=='__main__':
     if len(sys.argv)>1:

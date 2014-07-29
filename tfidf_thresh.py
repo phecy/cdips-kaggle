c82f17280988c15adfd923f6bb03b3285c3ab7c8
Thomas Stevens <tvsteven@gmail.com>
2014-07-28 21:12:38 -0700

    Dict order upon reduction was reversed

--- tfidf_thresh.py
+++ tfidf_thresh.py
@@ -16,7 +16,10 @@ from sklearn.externals import joblib
 import sys
 
 def write_hist(tfidf_sum,fname):
-    plt.hist(tf_idfsum,bins=np.sqrt(len(tf_idfsum)))
+    plt.hist(np.log10(tfidf_sum),bins=np.sqrt(len(tfidf_sum)))
+    plt.xlabel('log(TFIDF)')
+    plt.ylabel('Counts')
+    plt.title('Training Data Importance')
     plt.savefig(fname)
 
 def elim_exp_zeros(ngram_train):
@@ -26,40 +29,40 @@ def elim_exp_zeros(ngram_train):
     non0 = ngram_coo.data>0
     return sparse.coo_matrix((ngram_coo.data[non0],(ngram_coo.row[non0],ngram_coo.col[non0])), shape=ngram_coo.shape).tocsc()
 
-def thresh_elim_cols(ngram_train,featureIndex,threshold=0):
-    keep_idx = np.array(ngram_train.sum(axis=0).tolist()[0]) > threshold
+def thresh_elim_cols(mat,featureIndex,threshold=None):
+    if not threshold:
+        threshold = mat.min()
+    keep_idx = np.array(mat.sum(axis=0).tolist()[0]) >= threshold
     features,indices = zip(*sorted(featureIndex.iteritems(), key=operator.itemgetter(1)))
-    nzIndex = dict((k,i) for k,i in enumerate(np.array(features)[keep_idx]))
-    return ngram_train[:,keep_idx], nzIndex
+    nzIndex = dict((k,i) for i,k in enumerate(np.array(features)[keep_idx]))
+    return mat[:,keep_idx], nzIndex, keep_idx
    
-def main(threshold,feature_pkl='Jul27-15h27m/train_data.pkl'):
-    threshold = float(threshold)
+def main(feature_pkl='Jul27-15h27m/train_data.pkl',threshold=None):
+    if threshold:
+        threshold = float(threshold)
     print 'Loading features pickle...'
     featureIndex, trainFeatures, trainTargets, trainItemIds, testFeatures, testItemIds = joblib.load(feature_pkl)
     # Exclude non-ngram features
     ngram_train = trainFeatures[:,:-len(NEW_FEATURE_LIST)]
-    #--
-    ipdb.set_trace()
-    #--
     # Eliminate explicit zeros and uniformly zero columns
     ngram_train = elim_exp_zeros(ngram_train)
-    ngram_train, nzIndex = thresh_elim_cols(ngram_train,featureIndex,0)
+    ngram_train, nzIndex, tmp = thresh_elim_cols(ngram_train,featureIndex,0)
     # Calculate TF-IDF
     tfidf = DimReduction(ngram_train,'tfidf')
     tfidf_sum = np.array(tfidf.sum(axis=0).tolist()[0])
     write_hist(tfidf_sum,'train_tfidf_hist.png')
     # threshold the columns on TF-IDF sums
-    keep_idx, reducedIndex = thresh_elim_cols(tfidf_sum,nzIndex,threshold)
+    tmp, reducedIndex, keep_idx = thresh_elim_cols(np.matrix(tfidf_sum),nzIndex,threshold)
     # Stack the reduced features to the non-gram columns
-    trainReduced = sparse.hstack(trainFeatures[:,keep_idx],trainFeatures[:,len(NEW_FEATURE_LIST):])
-    testReduced = sparse.hstack(testFeatures[:,keep_idx],testFeatures[:,len(NEW_FEATURE_LIST):])
+    trainReduced = sparse.hstack((trainFeatures[:,keep_idx], trainFeatures[:,-len(NEW_FEATURE_LIST):]))
+    testReduced = sparse.hstack((testFeatures[:,keep_idx], testFeatures[:,-len(NEW_FEATURE_LIST):]))
     # Add non-ngram feature labels
     end = len(reducedIndex)
-    for label,i in enumerate(NEW_FEATURE_LIST):
-        reducedIndex[label]=end+i
+    for i,label in enumerate(NEW_FEATURE_LIST):
+        reducedIndex[label]=end+int(i)
     # Write new output pkl
-    out_pkl = os.path.splitext(feature_pkl)+'_tfidf-thresh.pkl'
-    joblib.dump(out_pkl,(reducedIndex, trainReduced, trainTargets, trainItemIds, testReduced, testItemIds))
+    out_pkl = os.path.splitext(feature_pkl)[0]+'_tfidf.pkl'
+    joblib.dump((reducedIndex, trainReduced, trainTargets, trainItemIds, testReduced, testItemIds),out_pkl)
     print 'Writing feature names...'
     write_featureIndex(reducedIndex,os.path.splitext(feature_pkl)+'_featlist_thresh.tsv')
 

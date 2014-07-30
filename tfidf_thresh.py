b2b40a13d8abd9c501f287d5a1a5e7e33c27dc14
Thomas Stevens <tvsteven@gmail.com>
2014-07-30 03:14:43 -0700

    featureIndex write error fixed, checked full script in ipdb

--- tfidf_thresh.py
+++ tfidf_thresh.py
@@ -47,10 +47,10 @@ def calc_tfidf(feat_mat,featureIndex):
     ngram_mat = elim_exp_zeros(ngram_mat)
     print 'Ngrams, no explicit zeros',ngram_mat.shape
     ngram_mat, nzIndex, tmp = thresh_elim_cols(ngram_mat,featureIndex,1)
-    print 'Ngrams, no uniform zero cols',ngram_mat.shape,len(nzIndex)
-    # Calculate TF-IDF --getting off-by one column drop
+    print 'Ngrams, no uniform zero cols (nor 1-element cols)',ngram_mat.shape,len(nzIndex)
+    # Calculate TF-IDF --NOTE: Prints off-by-one column drop, but matrix is correct shape
     tfidf = DimReduction(ngram_mat,'tfidf')
-    print 'TFIDF',tfidf.shape
+    print 'TFIDF',tfidf.shape,'previous:',ngram_mat.shape,len(nzIndex)
     #tfidf_sum = np.array(tfidf.sum(axis=0).tolist()[0])
     #write_hist(tfidf_sum,'train_tfidf_hist.png')
     # Stack the reduced features to the non-ngram columns
@@ -63,26 +63,13 @@ def main(feature_pkl='Jul27-15h27m/train_data.pkl',threshold=None):
         threshold = float(threshold)
     print 'Loading features pickle...'
     featureIndex, trainFeatures, trainTargets, trainItemIds, testFeatures, testItemIds = joblib.load(feature_pkl)
-    #______________
-    ipdb.set_trace()
-    #``````````````
     print 'POOL TRAIN/TEST:'
     allFeatures = sparse.vstack((trainFeatures,testFeatures),format='csc')
     print trainFeatures.shape,testFeatures.shape,allFeatures.shape
     allFeatures, reducedIndex = calc_tfidf(allFeatures,featureIndex)
-    allFeatures, reducedIndex, tmp = thresh_elim_cols(allFeatures,reducedIndex,0)
-    print 'TFIDF, no uniform zero cols',allFeatures.shape,len(reducedIndex)
     allFeatures = allFeatures.tocsr()
     trainFeatures = allFeatures[:trainFeatures.shape[0],:].tocsc()
     testFeatures = allFeatures[trainFeatures.shape[0]:,:].tocsc()
-
-    #print 'TRAIN:'
-    #trainFeatures, reducedIndex = calc_tfidf(trainFeatures,featureIndex)
-    #print 'TEST:'
-    #testFeatures, tmp = calc_tfidf(testFeatures,featureIndex)
-    # Remove uniformly zero columns after TF-IDF train, keep only those cols in test
-    #trainFeatures, reducedIndex, keep_idx = thresh_elim_cols(trainFeatures,reducedIndex,0)
-    #testFeatures = testFeatures[:,keep_idx]
 
     # Add non-ngram feature labels
     end = len(reducedIndex)
@@ -93,7 +80,7 @@ def main(feature_pkl='Jul27-15h27m/train_data.pkl',threshold=None):
     out_pkl = os.path.splitext(feature_pkl)[0]+'_tfidf.pkl'
     joblib.dump((reducedIndex, trainFeatures, trainTargets, trainItemIds, testFeatures, testItemIds),out_pkl)
     print 'Writing feature names...'
-    write_featureIndex(reducedIndex,os.path.splitext(feature_pkl)+'_featlist_thresh.tsv')
+    write_featureIndex(reducedIndex,os.path.splitext(feature_pkl)[0]+'_featlist_thresh.tsv')
 
 if __name__=='__main__':
     if len(sys.argv)>1:

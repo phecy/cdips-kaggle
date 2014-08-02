885531beaa79daa7fde62cbbebc5e6f3d3b1e6c6
Thomas Stevens <tvsteven@gmail.com>
2014-08-02 08:24:16 -0700

    fixed writing paths

--- cross_validation.py
+++ cross_validation.py
@@ -112,9 +112,8 @@ def main(feature_pkl='C:\\Users\Cory\\Documents\\DataScienceWorkshop\\avito_kagg
     ax.set_yticklabels([''] + labels)
     plt.xlabel('Predicted')
     plt.ylabel('True')
-    plt.savefig(os.path.splitext(feature_pkl)[0]+'_confusion.png')
-    plt.savefig(os.path.splitext(feature_pkl)[0]+'_confusion.pdf')
-    plt.savefig(os.path.splitext(feature_pkl)[0]+'_confusion.jpg')
+    for ext in ['.png','.pdf','.jpg']:
+        plt.savefig('confusion'+ext)
     
     #Add confusion matrix values to the graph
     width = len(norm_conf)
@@ -134,9 +133,8 @@ def main(feature_pkl='C:\\Users\Cory\\Documents\\DataScienceWorkshop\\avito_kagg
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('Receiver operating characteristic')
-    plt.savefig(os.path.splitext(feature_pkl)[0]+'_roc.png')
-    plt.savefig(os.path.splitext(feature_pkl)[0]+'_roc.pdf')
-    plt.savefig(os.path.splitext(feature_pkl)[0]+'_roc.jpg')
+    for ext in ['.png','.pdf','.jpg']:
+        plt.savefig('roc'+ext)
     
     auc_score = metrics.auc(mean_fpr,mean_tpr)
     print "AUC score\n" + str(auc_score)
